03afcf948645a3d59d761a986c9a76e3e5032d55
Schwarzchild-Entanglement <110633330+Schwarzchild-Entanglement@users.noreply.github.com>
2025-08-07 00:15:49 +0500

    made function for playing game round

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function getComputerChoice () {
     let ComputerChoice;
     let number = Math.random();
     if (number >= 0 && number <= 0.33)
-        ComputerChoice = "Rock";
+        ComputerChoice = "rock";
     else if (number >= 0.33 && number <= 0.66)
-        ComputerChoice = "Paper";
+        ComputerChoice = "paper";
     else if (number >= 0.66 && number < 1)
-        ComputerChoice = "Scissors";
+        ComputerChoice = "scissors";
     return ComputerChoice;
 }
 
@@ -19,4 +19,45 @@ function getHumanChoice () {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! rock beats scissors");
+        humanScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! scissors beats paper")
+        computerScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "scissors")
+        console.log("Nobody wins!");
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! paper beats rock");
+        computerScore++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "paper") {
+        console.log("Nobody wins!")
+    }
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! scissors beat paper");
+        humanScore++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "rock") {
+        console.log("Nobody wins!");
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! paoer beats rock");
+        humanScore++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You win! scissors beat paper");
+        humanScore++;
+    }
+}
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+alert(humanScore);
+alert(computerScore);
